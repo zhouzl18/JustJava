484ea690ce8e494fb6c9c0a47e3672d8dab0119f
zhouzl18 <fenglianhai18@gmail.com>
2016-04-26 01:13:37 +0800

    add increment button and decrement button.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,21 +8,46 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     /**
      * This method is called when the order button is clicked.
      * @param view
      */
-    public void submitOrders(View view){
-        int quantityOfCoffees = 2;
-        display(quantityOfCoffees);
-        displayPrice(quantityOfCoffees * 5);
+    public void submitOrder(View view){
+        //display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when the increment button is clicked.
+     * @param view
+     */
+    public void increment(View view){
+        quantity += 1;
+        display(quantity);
+        //displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when the decrement button is clicked.
+     * @param view
+     */
+    public void decrement(View view){
+        if(quantity > 1){
+            quantity -= 1;
+        }
+        display(quantity);
+        //displayPrice(quantity * 5);
     }
 
     /**
